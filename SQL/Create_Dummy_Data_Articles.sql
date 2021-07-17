489c80e12c46e70f481edc018a545ab4de36d038
SJ Kim <jincrom@gmail.com>
2021-07-18 00:15:12 +0900

    Add Dummy Data Articles

--- SQL/Create_Dummy_Data_Articles.sql
+++ SQL/Create_Dummy_Data_Articles.sql
@@ -2,10 +2,11 @@ WITH name_list AS (
 	SELECT '{"김승진", "장자미", "김선우", "김준우"}'::VARCHAR[] lname
 )
 
-INSERT INTO public."Articles"
+INSERT INTO public."Articles"(
+	"Id", "Name", "Description", "RegDate")
 SELECT 
 	n, 
 	lname[1 + mod(n, array_length(lname, 1))] || n as sname,
 	lname[1 + mod(n, array_length(lname, 1))] || n || '입니다. 안녕하세요.',
 	LOCALTIMESTAMP
-FROM name_list, generate_series(11, 100000) as n
+FROM name_list, generate_series(1, 20000) as n
