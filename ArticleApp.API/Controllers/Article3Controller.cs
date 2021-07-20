54663f2dd4522c0fed3f2949c7a8326680a03a9c
SJ Kim <jincrom@gmail.com>
2021-07-20 20:29:37 +0900

    DataTableResponse 전용 Controller 추가

---
+++ ArticleApp.API/Controllers/Article3Controller.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ArticleApp.API.Data;
+using ArticleApp.API.Models;
+using Microsoft.Extensions.Logging;
+
+namespace ArticleApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Article3Controller : ControllerBase
+    {
+        private readonly ILogger<Article3Controller> _logger;
+        private readonly BaseDbContext _context;
+        
+
+        public Article3Controller(ILogger<Article3Controller> logger, BaseDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        // GET: api/Article
+        [HttpGet]
+        public async Task<ActionResult<DataTableResponse>> GetArticles()
+        {
+            var articles =  await _context.Articles.ToListAsync();
+
+            return new DataTableResponse
+            {
+                RecordsTotal = articles.Count(),
+                RecordsFiltered = 10,
+                Data = articles.ToArray()
+            };
+        }
+
+        // GET: api/Article/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Article>> GetArticle(int id)
+        {
+            var article = await _context.Articles.FindAsync(id);
+
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            return article;
+        }
+
+        // PUT: api/Article/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutArticle(int id, Article article)
+        {
+            if (id != article.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(article).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ArticleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Article
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Article>> PostArticle(Article article)
+        {
+            _context.Articles.Add(article);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetArticle", new { id = article.Id }, article);
+        }
+
+        // DELETE: api/Article/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteArticle(int id)
+        {
+            var article = await _context.Articles.FindAsync(id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            _context.Articles.Remove(article);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ArticleExists(int id)
+        {
+            return _context.Articles.Any(e => e.Id == id);
+        }
+    }
+}
